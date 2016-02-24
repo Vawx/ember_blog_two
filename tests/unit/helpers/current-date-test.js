728c6b698737407f1d13a59023dfabdb7cee6bd8
Kyle Langley and Mike Mahoney <mahonmr@me.com>
2016-02-24 14:42:09 -0800

    add moment helper

---
+++ tests/unit/helpers/current-date-test.js
@@ -0,0 +1,10 @@
+import { currentDate } from 'blog/helpers/current-date';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | current date');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = currentDate([42]);
+  assert.ok(result);
+});
